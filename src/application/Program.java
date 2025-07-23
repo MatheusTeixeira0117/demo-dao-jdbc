bf8205d51a9db1cfaa5b05fe2cc66a660d48f750
Matheus Teixeira <matheus_teixeira2506@outlook.com>
2025-07-22 23:03:23 -0300

    update and deleteById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 	
@@ -36,6 +39,22 @@ public class Program {
 		Seller newSeller = new Seller(null, "Larissa", "larissa@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller);
+		
+		System.out.println("\n=== TEST 5: Seller upDate ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+	    sellerDao.update(seller);
+	    System.out.println("Ok! Update carried out.");
+	    
+	    System.out.println("\n=== TEST 6: Seller deleteId===");
+	    System.out.println("Enter id for delete test: ");
+	    int id = sc.nextInt();
+	    sellerDao.deleteById(id);
+	    System.out.println("Delete completed!");
+	    
+	    
+	    sc.close();
+	    
 	}
 
 }
